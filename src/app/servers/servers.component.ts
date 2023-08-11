1574e9f74a78341b7da06357a1b9084e38c4f67c
Ebe-N <141999434+Ebe-N@users.noreply.github.com>
2023-08-11 11:30:12 +0300

    Add files via upload

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+
+@Component({
+selector: 'app-servers',
+// template: `
+// <app-server></app-server>
+// <app-server></app-server>`,
+templateUrl: './servers.component.html',
+styleUrls: ['./servers.component.css']
+
+})
+
+export class ServersComponent{
+allowNewServer = false;
+serverCreationStatus = 'No Server was Created'
+serverName = 'Secret password = Tuna';
+serverCreated = false; 
+servers = ['']
+
+
+constructor() {
+    setTimeout(() => {
+            this.allowNewServer = true;
+        }
+     ,2000)
+}
+
+ onCreateServer() {
+    this.serverCreated =true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+
+}
+
+onUpdateServerName(event: any) {
+this.serverName = (<HTMLInputElement>event.target).value;
+}
+}
